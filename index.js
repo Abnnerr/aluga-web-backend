52dfe01e93e564ae4944303f7fe79eac3a296640
Abnnerr <jonnas.ab.00@gmail.com>
2025-06-12 14:59:46 -0300

    updates

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = 8000;
 
-const usuariosRoutes = require('./src/routes/usuariosRoutes.js')
+const usuariosRoutes = require('./src/routes/usuariosRoutes.js');
+const { cadastro } = require("./src/controllers/usuariosController.js");
 
+app.use(cors())
 app.use(express.json())
-
-
-app.use("/usuarios", usuariosRoutes)
+app.post("/cadastro", async (req,res) => {
+    res.send(await  cadastro(req.body))
+})
 
 app.listen(port, () => {
     console.log(`servidor de pé: http://localhost:${port}`)
--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,38 +1,59 @@
-const { prisma } = require("../services")
+const { prisma } = require("../services");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken")
+const saltRolts = 10;
 
-async function buscarUsuarios() {
 
-    return await prisma.usuarios.findMany({})
+async function cadastro(dados) {
 
+
+    try {
+        let senhaCriptografada = await bcrypt.hash(dados.usuario_senha, saltRolts)
+        dados = {...dados, usuario_senha: senhaCriptografada}
+        
+        const existe = await prisma.usuarios.findFirst({
+            where: {
+                usuario_email: dados.usuario_email
+            }
+        })
+        if (existe) {
+            return {
+                tipo: "warning",
+                message: "email ja existe"
+            }
+        }
+        return await prisma.usuarios.create({
+            data: dados
+        })
+    } catch (error) {
+        return {
+            tipo: "erro",
+            message: error.message
+        }
+    }
 }
 
-async function busccarUsuario() {
+// async function login(dados) {
 
-    
-    
-}
+//     try {
+//         let usuario = await prisma.usuarios.findFirst( {
+//             where: {
+//                 usuario_email: dados.usuario_email
+//             }
+//         })
+//         if (usuario) {
+//             let verificarSenha = bcrypt.compare(dados.usuario_senha, usuario.usuario_senha)
 
-async function criarUsuario(dados) {
+//             if (verificarSenha) {
+//                 let token = jwt.sign({usuario.usuario_senha})
+//             }
+//         }
+//     } catch (error) {
+        
+//     }
+// }
 
-    return await prisma.usuarios.create({
-        data: dados
-    })
-
-}
-async function editarUsuario(id, dados) {
-
-    return await prisma.usuarios.update({
-
-        where: {
-            usuario_id: Number(id),
-        },
-        data: dados
-    })
-
-}
 
 module.exports = {
-    buscarUsuarios,
-    criarUsuario,
-    editarUsuario
+    cadastro
 }
