ff229bbeb9fb7033210cb476101be97a941a39c5
Abnnerr <jonnas.ab.00@gmail.com>
2025-06-13 01:27:48 -0300

    update

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -49,8 +49,7 @@ async function login(dados) {
 
                 return {
                     usuario,
-                    token,
-                    usuario
+                    token
                 }
             }
         }
