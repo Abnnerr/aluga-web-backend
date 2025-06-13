9ecc8de6e43b009fbf4bba0648b04ec60edeb7c8
DarlisonBrandao <DarlisonBrandao123@gmail.com>
2025-06-13 09:56:01 -0300

    Create Rota Imoveis

---
+++ src/routes/imoveisRoutes.js
@@ -0,0 +1,25 @@
+
+
+const router = require("express").Router();
+
+const { buscarImoveis, criarImoveis , apagarImoveis } = require("../controller/imoveisController.js");
+const { rotaProtegida } = require("../utils/index.js");
+
+
+router.get("/", async (req, res) => {
+  res.send(await buscarImoveis());
+});
+
+router.post("/", rotaProtegida , async (req, res) => {
+  res.send( await criarImoveis(req.body));
+});
+router.put("/:id", rotaProtegida , async (req, res) => {
+  res.send(`Edita um imovel id: ${req.params.id}`);
+});
+router.delete("/:id", rotaProtegida , async (req, res) => {
+  res.send(await apagarImoveis(req.params.id));
+});
+
+
+
+module.exports = router;
