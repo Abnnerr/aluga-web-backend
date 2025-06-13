4543d52976d6aa58ac16813052dfa590ed8b6186
Abnnerr <jonnas.ab.00@gmail.com>
2025-06-13 16:39:31 -0300

    update

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -1,24 +1,24 @@
 
-const router = require("express").Router();
+// const router = require("express").Router();
 
-const {buscarUsuarios, criarUsuario, apagarUsuario } = require("../controller/usuariosController.js");
-const { rotaProtegida } = require("../utils/index.js");
-
-
-router.get("/", async (req, res) => {
-  res.send(await buscarUsuarios());
-});
-
-router.post("/", rotaProtegida , async (req, res) => {
-  res.send( await criarUsuario(req.body));
-});
-router.put("/:id", rotaProtegida , async (req, res) => {
-  res.send(`Edita um usuario id: ${req.params.id}`);
-});
-router.delete("/:id", rotaProtegida , async (req, res) => {
-  res.send(await apagarUsuario(req.params.id));
-});
+// const {buscarUsuarios, criarUsuario, apagarUsuario } = require("../controller/usuariosController.js");
+// const { rotaProtegida } = require("../utils/index.js");
 
 
+// router.get("/", async (req, res) => {
+//   res.send(await buscarUsuarios());
+// });
 
-module.exports = router;
+// router.post("/", rotaProtegida , async (req, res) => {
+//   res.send( await criarUsuario(req.body));
+// });
+// router.put("/:id", rotaProtegida , async (req, res) => {
+//   res.send(`Edita um usuario id: ${req.params.id}`);
+// });
+// router.delete("/:id", rotaProtegida , async (req, res) => {
+//   res.send(await apagarUsuario(req.params.id));
+// });
+
+
+
+// module.exports = router;
