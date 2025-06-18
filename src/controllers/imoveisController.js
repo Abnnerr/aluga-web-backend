c04887cc021c1e79dda0f7d794bd16c7d97b5ffc
Abnnerr <jonnas.ab.00@gmail.com>
2025-06-17 23:19:24 -0300

    updates

--- src/controllers/imoveisController.js
+++ src/controllers/imoveisController.js
@@ -1,25 +1,45 @@
-import { prisma } from "../services";
 
-const { executarSQL, prisma } = require("../services");
+const { prisma } = require('../services')
 
-async function buscarImoveis() {
+async function buscarImovel(dados) {
+
   try {
-    return await prisma.imoveis.findMany();
-  } catch (error) {
-    return {
-      tipo: "erro",
-      mensagem: error.message,
-    };
-  }
-}
+    const filtros = {};
 
-async function buscarUmImovel(id) {
-  try {
-    return await prisma.imoveis.findMany({
-      where: {
-        imoveis_id: Number(id),
-      },
+    if (dados.imovel_estado) {
+      filtros.imovel_estado = dados.imovel_estado;
+    }
+
+    if (dados.imovel_bairro) {
+      filtros.imovel_bairro = dados.imovel_bairro;
+    }
+
+    if (dados.imovel_modalidade) {
+      filtros.imovel_modalidade = dados.imovel_modalidade;
+    }
+
+    if (dados.imovel_tipo) {
+      filtros.imovel_tipo = dados.imovel_tipo;
+    }
+
+    if (dados.imovel_quarto) {
+      filtros.imovel_quarto = dados.imovel_quarto;
+    }
+
+    if (dados.imovel_garagem) {
+      filtros.imovel_garagem = dados.imovel_garagem;
+    }
+
+    if (dados.imovel_banheiro) {
+      filtros.imovel_banheiro = dados.imovel_banheiro;
+    }
+
+    const resultado = await prisma.imoveis.findMany({
+      where: filtros,
     });
+
+    return resultado;
+    
   } catch (error) {
     return {
       tipo: "erro",
@@ -28,28 +48,53 @@ async function buscarUmImovel(id) {
   }
 }
 
-const prisma = new PrismaClient();
+// async function criarImovel(dados) {
+//   return await prisma.imoveis.create({
+//     data: dados
+//   })
+// }
 
-async function criarImovel(dados) {
-  return await prisma.produto.create({
-    data: {
-      imoveis_nome: dados.imoveis_nome,
-      imoveis_preco: dados.imoveis_preco,
-      imoveis_desconto: dados.imoveis_desconto,
-      imoveis_imagem: dados.imoveis_imagem,
-      marca_id: dados.marca_id,
-      categoria_id: dados.categoria_id,
-    },
-  });
+// async function buscarUmImovel(id) {
+//     try {
+//         return await prisma.produtos.findUnique({
+//             where: { produtos_id: Number(id) }
+//         });
+//     } catch (error) {
+//         return {
+//             tipo: "erro",
+//             mensagem: error.message
+//         }
+//     }
+// }
+// async function editarImovel(id, dados) {
+//     // return await executarSQL(`UPDATE produtos SET produto_nome = '${dados.produto_nome}', produto_preco = ${dados.produto_preco}, produto_desconto = ${dados.produto_desconto}, produto_imagem = '${dados.produto_imagem}', marca_id = ${dados.marca_id}, categoria_id = ${dados.categoria_id} WHERE produtos_id = ${id};`);
+//     try {
+//         return await prisma.produtos.update( {
+//             where: {produtos_id: Number(id)},
+//             data: dados
+//         })
+//     } catch (error) {
+//         return {
+//             tipo: "erro",
+//             mensagem: error.message
+//         }
+//     }
+// }
+// async function apagarImovel(id) {
+//     try {
+//         return await prisma.produtos.delete({
+//             where: {
+//                 produtos_id: Number(id)
+//             }
+//         })
+//     } catch (error) {
+//         return {
+//             tipo: "erro",
+//             mensagem: error.message
+//         }
+//     }
+// }
+
+module.exports = { buscarImovel, 
+  // buscarUmImovel, criarImovel, editarImovel, apagarImovel 
 }
-
-async function apagarImovel(id) {
-  return await executarSQL(`DELETE FROM imoveis WHERE imovel_id = ${id};`);
-}
-
-module.exports = {
-  buscarImoveis,
-  buscarUmImovel,
-  criarImovel,
-  apagarImovel,
-};
--- src/routes/imoveisRoutes.js
+++ src/routes/imoveisRoutes.js
@@ -1,26 +1,31 @@
 
+const router = require("express").Router()
 
-// const router = require("express").Router();
+const { criarImoveis } = require("../controller/imoveisController.js");
 
-// const { buscarImoveis, criarImoveis , apagarImoveis } = require("../controller/imoveisController.js");
-// const { rotaProtegida } = require("../utils/index.js");
-
-
-// router.get("/", async (req, res) => {
-//   res.send(await buscarImoveis());
-// });
-
-// router.post("/", rotaProtegida , async (req, res) => {
-//   res.send( await criarImoveis(req.body));
-// });
-// router.put("/:id", rotaProtegida , async (req, res) => {
-//   res.send(`Edita um imovel id: ${req.params.id}`);
-// });
-// router.delete("/:id", rotaProtegida , async (req, res) => {
-//   res.send(await apagarImoveis(req.params.id));
-// });
+router.post("/", async (req, res) => {
+    res.send(await criarImoveis(req.body));
+});
 
 
+const { buscarImovel, criarImovel, editarImovel, apagarImovel } = require("../controller/produtosController")
 
 
-// module.exports = router;
+router.get("/", async (req, res) => {
+    res.send(await buscarImovel())
+
+})
+router.get("/:id", async (req, res) => {
+    res.send(await buscarImovel(req.params.id))
+})
+router.post("/", async (req, res) => {
+    res.send(await criarImovel(req.body))
+})
+router.put("/:id", async (req, res) => {
+    res.send(await editarImovel(req.params.id, req.body))
+})
+router.delete("/:id", async (req, res) => {
+    res.send(await apagarImovel(req.params.id))
+})
+
+module.exports = router;
